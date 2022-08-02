69bca817d4dcbf2e46b1a63262d3685230748b7d
nftchance <chance@utc24.io>
2022-08-02 10:41:43 -0500

    disabling eslint

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
     webpack: {
+        eslint: { 
+            enable: false
+        },
         module: {
             rules: [
                 {
