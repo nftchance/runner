a1b90d7ea97d90213ac116fb98599f6e37bf98a4
masonchain <masonlandrystacker@gmail.com>
2022-08-12 00:08:00 -0500

    lord finally got test environment working
    Pain in the ass to get everything resolving but its ready to go and can be fleshed out now.
    
    This commit just includes checking the render of every current static page.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Runner is currently in Beta/i);
-  expect(linkElement).toBeInTheDocument();
+import '@testing-library/jest-dom'
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+import App from "./App"
+it('can render App', async () => {
+  await waitFor(() => { 
+    ReactDOM.createRoot(container).render(<App />)
+  });
+});
+
+import Home from "@components/Home/Home"
+it('can render Home', async () => {
+  await waitFor(() => { 
+    ReactDOM.createRoot(container).render(<Home />)
+  });
+});
+
+
+import Roadmap from "@components/Roadmap/Roadmap"
+it('can render Roadmap', async () => {
+  await waitFor(() => { 
+    ReactDOM.createRoot(container).render(<Roadmap />)
+  });
+});
+
+import FAQ from "@components/FAQ/FAQ"
+it('can render FAQ', async () => {
+  await waitFor(() => { 
+    ReactDOM.createRoot(container).render(<FAQ />)
+  });
+});
+
+import Governance from "@components/Governance/Governance"
+it('can render Governance', async () => {
+  await waitFor(() => { 
+    ReactDOM.createRoot(container).render(<Governance />)
+  });
+});
+
+import Proposal from "@components/Governance/Proposal"
+it('can render Proposal', async () => {
+  await waitFor(() => { 
+    ReactDOM.createRoot(container).render(<Proposal />)
+  });
 });
